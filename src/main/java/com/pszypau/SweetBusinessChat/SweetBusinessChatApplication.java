fa9e42c8cd8655895c6f7ada59d97800c1d41f2f
Karol <karol44k@gmail.com>
2018-03-24 14:26:57 +0100

    poprawione sory piter

--- src/main/java/com/pszypau/SweetBusinessChat/SweetBusinessChatApplication.java
+++ src/main/java/com/pszypau/SweetBusinessChat/SweetBusinessChatApplication.java
@@ -2,6 +2,9 @@ package com.pszypau.SweetBusinessChat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -10,5 +13,11 @@ public class SweetBusinessChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SweetBusinessChatApplication.class, args);
+	}
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
