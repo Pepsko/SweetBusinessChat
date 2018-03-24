7a125e2397a02a120666bb78561a05fd24e54010
Karol <karol44k@gmail.com>
2018-03-24 11:22:08 +0100

    changed ignore and sdk

--- src/main/java/com/pszypau/SweetBusinessChat/SweetBusinessChatApplication.java
+++ src/main/java/com/pszypau/SweetBusinessChat/SweetBusinessChatApplication.java
@@ -3,6 +3,7 @@ package com.pszypau.SweetBusinessChat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SweetBusinessChatApplication {
 
