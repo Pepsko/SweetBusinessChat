e85964eeaf6f1c3bcd539ab525d45e5b8f047f7c
Pepsko <pepskoo96@gmail.com>
2018-03-24 11:13:59 +0100

    First commit

---
+++ src/main/java/com/pszypau/SweetBusinessChat/SweetBusinessChatApplication.java
@@ -0,0 +1,12 @@
+package com.pszypau.SweetBusinessChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetBusinessChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetBusinessChatApplication.class, args);
+	}
+}
