37ed83a4884bb2cb5e86ac758745559fec764ca2
Karol <karol44k@gmail.com>
2018-03-24 13:26:26 +0100

    aaa

--- src/main/java/com/pszypau/SweetBusinessChat/Entity/UserEntity.java
+++ src/main/java/com/pszypau/SweetBusinessChat/Entity/UserEntity.java
@@ -1,13 +1,17 @@
 package com.pszypau.SweetBusinessChat.Entity;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,19 +20,51 @@ public class UserEntity {
     private String uuid = UUID.randomUUID().toString();
     @NotBlank
     private String name;
-    @NotBlank
-    private String surname;
     @Email
     @NotBlank
     private String email;
     @NotBlank
     private String password;
 
-    public UserEntity(@NotBlank String uuid, @NotBlank String name, @NotBlank String surname, @Email @NotBlank String email, @NotBlank String password) {
+    public UserEntity(@NotBlank String uuid, @NotBlank String name, @Email @NotBlank String email, @NotBlank String password) {
         this.uuid = uuid;
         this.name = name;
-        this.surname = surname;
         this.email = email;
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
